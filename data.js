0e988d596658ea8f1ccea47604de0302f8dfe597
Palguni Samaga <113446923+Pal-Sam@users.noreply.github.com>
2022-09-24 16:03:32 +0530

    hello added

--- data.js
+++ data.js
@@ -5,5 +5,11 @@ export const Data =[
         branch: "CSE",
         year: "3rd",
     },
+    {
+        name: "Palguni SAMAGA",
+        username: "PAL-SAM",
+        branch: "CSE",
+        year: "3rd",
+    },
     
 ];
